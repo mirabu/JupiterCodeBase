68d22589d4386e4dacdfc04f3ac225137351adc8
jugalbadgujar <astonmar43@gmail.com>
2026-06-20 14:45:31 +0530

    ok

--- src/Arrays/Fibonacii.java
+++ src/Arrays/Fibonacii.java
@@ -1,7 +1,9 @@
 package Arrays;
 
 public class Fibonacii {
-    public static void main(String[] args) {
+    static void main() {
+
+
         int n1=0, n2=1, n3, i, count=10;
         System.out.println(n1+ " "+n2);
 
